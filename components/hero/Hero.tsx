e3c8e3c482fe2a77e27fd78207ec365097de97e9
Hazem <hazemhusseinv7@gmail.com>
2025-09-16 01:55:11 +0300

    Added Hero component

---
+++ components/hero/Hero.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import { TextEffect } from "@/components/motion-primitives/text-effect";
+import SplashCursor from "@/components/hero/SplashCursor";
+import HeroAnimation from "@/components/hero/HeroAnimation";
+
+const Hero = () => {
+  return (
+    <section className="relative">
+      <SplashCursor />
+      <div className="w-full px-20 py-4 absolute left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center text-center">
+        <Image
+          className="w-40 lg:w-100 max-w-3/4"
+          src="/logo/logo.svg"
+          width={512}
+          height={512}
+          alt="Logo"
+          priority
+        />
+
+        <div className="flex flex-col space-y-2 font-semibold text-xl sm:text-4xl md:text-5xl lg:text-7xl -mt-4 lg:-mt-20">
+          <TextEffect per="word" preset="blur" delay={0.5} as="h1">
+            أعمال تناسب منتجك
+          </TextEffect>
+          <TextEffect per="word" preset="blur" delay={1.5}>
+            وحملات تُحدث الأثر
+          </TextEffect>
+        </div>
+      </div>
+
+      <HeroAnimation />
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/hero/HeroAnimation.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CrowdCanvas } from "@/components/ui/skiper-ui/skiper39";
+
+const HeroAnimation = () => {
+  const [scale, setScale] = useState(0.4);
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window === "undefined") return;
+
+      const width = window.innerWidth;
+      if (width < 640) setScale(0.2);
+      else if (width < 768) setScale(0.25);
+      else if (width < 1024) setScale(0.3);
+      else if (width < 1280) setScale(0.35);
+      else setScale(0.4);
+    };
+
+    if (typeof window !== "undefined") {
+      handleResize();
+      window.addEventListener("resize", handleResize);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="relative h-screen w-full">
+      <CrowdCanvas src="/hero/peeps.png" rows={12} cols={9} scale={scale} />
+    </div>
+  );
+};
+
+export default HeroAnimation;
